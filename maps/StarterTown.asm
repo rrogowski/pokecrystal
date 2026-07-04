9d7666cc4c6bdc8509529cf90a78fc84a7c65739
Stone Yeatts <stone.yeatts@gmail.com>
2026-07-03 21:55:26 -0400

    add BGEVENT Starter Town Salt Lab Sign Text

--- maps/StarterTown.asm
+++ maps/StarterTown.asm
@@ -5,8 +5,16 @@ StarterTown_MapScripts:
 
 	def_callbacks
 
+StarterTownLabSign:
+	jumptext StarterTownLabSignText
+
+StarterTownLabSignText:
+	text "SALT #LAB"
+	done
+
 StarterTown_MapEvents:
 	db 0, 0 ; filler
+
 
 	def_warp_events
 	warp_event  29, 13, STARTER_TOWN_PLAYERS_HOUSE, 1
@@ -15,5 +23,6 @@ StarterTown_MapEvents:
 	def_coord_events
 
 	def_bg_events
+    bg_event 24,  8, BGEVENT_READ, StarterTownLabSign 
 
 	def_object_events
--- maps/StarterTownPlayersHouse.asm
+++ maps/StarterTownPlayersHouse.asm
@@ -8,6 +8,7 @@ StarterTownPlayersHouse_MapScripts:
 StarterTownPlayersHouse_MapEvents:
 	db 0, 0 ; filler
 
+
 	def_warp_events
 	warp_event 4, 7, STARTER_TOWN, 1
 	warp_event 5, 7, STARTER_TOWN, 1
@@ -15,5 +16,6 @@ StarterTownPlayersHouse_MapEvents:
 	def_coord_events
 
 	def_bg_events
+	
 
 	def_object_events
